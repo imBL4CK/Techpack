53c5894972a185514488f0c2b4cfebcab5c042bd
BL4CK_ <160023261+imBL4CK@users.noreply.github.com>
2024-02-14 17:28:07 -0300

    Atualizar o temporary.js

--- temporary.js
+++ temporary.js
@@ -4,40 +4,20 @@ ServerEvents.recipes(event => {
   igredients:[{item: ""}]
   result: [{}]
  })
-})
-//ethernal stella
-{
-  "additional_requirements": {
-    "forge_tier": 1
-  },
-  "essences": {
-    "aureal":25,
-    "blood": 250,
-    "experience": 0,
-    "souls": 1
-  },
-  "inputs": [
-    {
-      "amount": 3,
-      "ingredient": {
-        "item": thermal:redstone_coil"
-      }
-    },
-    {
-      "amount": 1,
-      "ingredient": {
-        "item": "forbidden_arcanus:stellarite_piece"
-      }
-    }
-  ],
-  "main_ingredient": {
-    "item": "minecraft:diamond"
-  },
-  "result": {
-    "type": "forbidden_arcanus:create_item",
-    "result_item": {
-      "Count": 1,
-      "id": "gi"
-    }
+
+ event.custom({
+  type: "createindustry:casting",
+  igredients: [
+  {
+    fluid: "#forge:molten_bronze", 
+    amount: 1,
   }
-}
+ ],
+ processingTime: 300,
+ results: [
+ {
+   item: "#forge:ingots/bronze", count: 1,
+ }
+]
+ })
+})
