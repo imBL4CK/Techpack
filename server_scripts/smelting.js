1b543c6b52b0a24f0ccc3df1500adb1141f40503
BL4CK_ <160023261+imBL4CK@users.noreply.github.com>
2024-02-19 11:53:32 -0300

    Atualizar o smelting.js

--- server_scripts/smelting.js
+++ server_scripts/smelting.js
@@ -1,6 +1,6 @@
 ServerEvents.recipes(event => {
    event.smelting(
-  Igredient: {'eidolon:pewter_nugget'},
+  igredient: {'eidolon:pewter_nugget'},
   results: {'kubejs:hot_pewter_nugget'}
  )
 })
