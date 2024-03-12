9db1480213989875322a4d79c2c4186a7a64d0d6
BL4CK_ <carlosfigueiredorex@gmail.com>
2024-03-11 23:05:52 -0300

    🚧 WIP:

--- startup_scripts/registry/items.js
+++ startup_scripts/registry/items.js
@@ -1,4 +1,14 @@
-  //Items
+// ╭— 📌 Ideias
+
+//Stainless Steel
+//Mica
+//Insulated Foil
+//Imput Bus Component
+//output Bus Component
+//Tungstensteel
+
+
+//Items
 StartupEvents.registry('item', event =>{
 //Wooden Gear
     event.create('wooden_gear').displayName('Wooden Gear')
@@ -33,12 +43,13 @@ StartupEvents.registry('item', event =>{
     .maxDamage(80)
     .attackDamageBaseline(1)
 //Primitive Wrench
-    event.create('primitive_wrench')
+    event.create('primitive_wrench','pickaxe')
     .displayName('Primitive Wrench')
     .maxStackSize(1)
     .tier ('iron',)
     .maxDamage(80)
     .attackDamageBaseline(1)
+
 //Makesift Mecanism
     event.create('makeshift_mechanism')
     .displayName('Makeshif Mechanism')
@@ -90,10 +101,8 @@ event.create('blue_alloy_wire')
 
 //Aluminium Brass Ingot
  event.create('aluminium_brass_ingot').displayName('Aluminium Brass Ingot')
-
 //Aluminium Brass Plate
  event.create('aluminium_brass_plate').displayName('Aluminium Brass Plate')
-
 //Aluminuum Brass Gear
  event.create('aluminium_brass_gear').displayName('Aluminium Brass Gear')
 
@@ -102,11 +111,9 @@ event.create('blue_alloy_wire')
 //Draconium Ingot
  event.create('draconium_ingot')
 .displayName('Draconium Ingot')
-
 //Draconium Plate
  event.create('draconium_plate')
 .displayName('Draconium Plate')
-
 //Draconium Gear
 event.create('draconium_gear')
 .displayName('Draconium Gear')
@@ -116,113 +123,84 @@ event.create('draconium_gear')
 //Melodic Alloy
  event.create('melodic_alloy_ingot')
 .displayName('Melodic Alloy Ingot')
-
 //Crystaline Pink Slime
  event.create('crystalline_pink_slime_ingot').displayName('Crystalline Pink Slime Ingot')
-
 //Vivid Alloy 
  event.create('vivid_alloy_ingot')
 .displayName('Vivid Alloy Ingot')
-
 //Energetic Silver Ingot
  event.create('energetic_silver_ingot')
 .displayName('Energetic Silver Ingot')
-
 //Crystaline Alloy
  event.create('crystalline_alloy_ingot')
 .displayName('Crystalline Alloy Ingot')
-
 //Stellar Alloy
  event.create('stellar_alloy_ingot')
 .displayName('Stellar Alloy Ingot')
-
 //Crude Steel
  event.create('crude_steel_ingot')
 .displayName('Crude Steel Ingot')
-
 // Hot Ferrous pewter
 event.create('hot_ferrous_pewter_ingot')
 .displayName('Hot Ferrous Pewter Ingot')
-
 //Ferrous pewter nugget
 event.create('hot_ferrous_pewter_nugget')
 .displayName('Hot Ferrous Pewter nugget')
-
 //Ferrous Pewter
 event.create('ferrous_pewter_ingot')
 .displayName('Ferrous Pewter Ingot')
-
 //Ferrous Pewter Plate
 event.create('ferrous_pewter_plate')
 .displayName('Ferrous Pewter Plate')
-
 //Ferrous Pewter Nugget
 event.create('ferrous_pewter_nugget')
 .displayName('Ferrous Pewter Nugget')
-
 //Conductive blend
 event.create('conductive_blend')
 .displayName('Conductive Blnd')
-
 //Primitive Circuit
 event.create('primitive_circuit')
 .displayName('Primitive Circuit')
-
 //Primitive Circuit Board
 event.create('primitive_circuit_board')
 .displayName('Primitive Circuit Board')
-
 //Primitive IO Component
 event.create('primitive_io_component')
 .displayName('Primitive IO Component')
-
 //Pewter Plate
 event.create('pewter_plate')
 .displayName('Pewter Plate')
-
 //Ink and feather
 event.create('ink_and_feather')
 .displayName('Ink and Feather')
-
 //Arcane Runic Slate
 event.create('empty_arcane_runic_slate')
 .displayName('Arcane Runic Slate')
-
 //Manganese ingot
 event.create('manganese_ingot')
 .displayName('Manganese ingot')
-
 //Manganese Dust
 event.create('manganese_dust')
 .displayName('Manganese Dust')
-
 //Tungsten Ingot
 event.create('tungsten_ingot')
 .displayName('Tungsten Ingot')
-
 //Alloy Mixture
 event.create('alloy_mixture')
 .displayName('Alloy Mixture')
-
 //Aluminium Plate
 event.create('aluminium_plate')
 .displayName('Aluminium Plate')
 
-//Stainless Steel
-//Mica
-//Insulated Foil
-//Imput Bus Component
-//output Bus Component
+// -x-
 
 //Basic Wrench (Steel tier)
-event.create('basic_wrench')
+event.create('basic_wrench','pickaxe')
 .displayName('Basic Wrench')
 .maxStackSize(1)
 .tier ('iron',)
 .maxDamage(316)
-.attackDamageBaseline(1)
 .attackDamageBonus(9)
-
 //Basic Saw (Steel Tier)
 event.create('basic_saw', 'axe')
 .displayName('Basic Saw')
@@ -230,7 +208,6 @@ event.create('basic_saw', 'axe')
 .tier ('iron',)
 .maxDamage(316)
 .attackDamageBaseline(1)
-
 //Basic Hammer (Steel Tier)
 event.create('basic_hammer', 'pickaxe')
 .displayName('Basic Hammer')
@@ -238,7 +215,6 @@ event.create('basic_hammer', 'pickaxe')
 .tier ('iron',)
 .maxDamage(316)
 .attackDamageBaseline(1)
-
 //Basic Wirecutter (Steel Tier)
 event.create('basic_wirecutter','sword')
 .displayName('Basic Wirecutter')
@@ -246,7 +222,6 @@ event.create('basic_wirecutter','sword')
 .tier ('iron',)
 .maxDamage(316)
 .attackDamageBaseline(1)
-
 //Basic Crowbar (Steel Tier)
 event.create('basic_crowbar','sword')
 .displayName('Basic Crowbar')
