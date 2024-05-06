1cd3a6e7c2cf151b0df6b83f33bba9065144dc78
BL4CK_ <160023261+imBL4CK@users.noreply.github.com>
2024-05-06 12:07:03 -0300

    materials test file

---
+++ server_scripts/materials.js
@@ -0,0 +1,18 @@
+ServerEvents.recipes(e =>{
+
+     var baseMaterials = [
+        'copper',
+        'iron',
+        'gold',
+        'tin',
+        'steel',
+        'bronze',
+        'invar',
+        'brass',
+        'aluminum',
+        'aluminum_brass',
+        'diamond',
+        'netherite',
+        'diamond'
+     ]
+})
