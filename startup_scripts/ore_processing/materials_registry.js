8138d2cfcb2c8a1e7686ff7727579eb09a2d74f1
BL4CK_ <160023261+imBL4CK@users.noreply.github.com>
2024-05-09 12:32:04 -0300

    Atualizar o materials_registry.js

--- startup_scripts/ore_processing/materials_registry.js
+++ startup_scripts/ore_processing/materials_registry.js
@@ -7,6 +7,11 @@ StartupEvents.registry('item', e =>{
         "",
     ]
     CustomMaterials.forEach((cRegistry) => {
+        //Ingots
         e.create(cRegistry +'_ingot').texture('kubejs:item/library/'+cRegistry+'_ingot')
+//Plates
+e.create(cRegistry +'_plate').texture('kubejs:item/library/'+cRegistry+'_plate')
+//Gears
+e.create(CRegistry +'gear').texture('kubejs:item/library/' +cRegistry+ 'gear')
     }
 })
