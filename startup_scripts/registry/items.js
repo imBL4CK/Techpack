334860995c55e1d74a2b6762aa8c3c2bdda551ac
BL4CK_ <carlosfigueiredorex@gmail.com>
2024-05-09 14:44:40 -0300

    update 9/5 .3

--- startup_scripts/registry/items.js
+++ startup_scripts/registry/items.js
@@ -86,112 +86,34 @@ event.create('thermo_mechanism')
 //Circuit Mechanism
 event.create('circuit_mechanism')
 .displayName('Circuit Mechanism')
-
 //Commum Techcoin
-    event.create('commum_techcoin')
-    .displayName('Commum Techcoin')
-    .rarity('common')
+event.create('commum_techcoin')
+.displayName('Commum Techcoin')
+.rarity('common')
 //Uncommum Techcoin
-    event.create('uncommum_techcoin')
-    .displayName('Uncommum Techcoin')
-    .rarity('uncommon')
+event.create('uncommum_techcoin')
+.displayName('Uncommum Techcoin')
+.rarity('uncommon')
 //Rare Techcoin
-    event.create('rare_techcoin').displayName('Rare Techcoin')
-    .rarity('rare')
+event.create('rare_techcoin').displayName('Rare Techcoin')
+.rarity('rare')
 //Legendary Techcoin
-    event.create('legendary_techcoin').displayName('Legendary Techcoin')
-    .rarity('legendary')
-
+event.create('legendary_techcoin').displayName('Legendary Techcoin')
+.rarity('legendary')
 //Golden Hand
-    event.create('golden_hand').displayName('Golden Hand')
+event.create('golden_hand').displayName('Golden Hand')
 //Wooden Gear
 event.create('wooden_gear').displayName('Wooden Gear')
 //Wooden Board
-    event.create('wooden_board').displayName('Wooden Board')
+event.create('wooden_board').displayName('Wooden Board')
 //Zinc Dust
-    event.create('zinc_dust').displayName('Zinc Dust')  
-
-    //Colored Alloys
-
-//Red Ally Ingot
- event.create('red_alloy_ingot').displayName('Red Alloy Ingot')
-//Red Alloy Plate
- event.create('red_alloy_plate').displayName('Red Alloy Plate')
-//Red Alloy Gear
- event.create('red_alloy_gear').displayName('Red Alloy Gear')
-//Red Alloy Wire
-event.create('red_alloy_wire').displayName('Red Alloy Wire')
-//Blue Alloy Ingot
-event.create('blue_alloy_ingot')
-.displayName('Blue Alloy Ingot')
-//Blue Alloy Plate
-event.create('blue_alloy_plate')
-.displayName('Blue Alloy Plate')
-//Blue Alloy Gear
-event.create('blue_alloy_gear')
-.displayName('Blue Alloy Gear')
-//Blue Alloy Wire
-event.create('blue_alloy_wire')
-.displayName('Blue Alloy Wire')
-
-    //thaumcraft
-
-//Aluminum Brass Ingot
- event.create('aluminum_brass_ingot').displayName('Aluminum Brass Ingot')
-//Aluminum Brass Plate
- event.create('aluminum_brass_plate').displayName('Aluminum Brass Plate')
-//Aluminum Brass Gear
- event.create('aluminum_brass_gear').displayName('Aluminum Brass Gear')
-
-    //Draconic Evolution
-
-//Draconium Ingot
- event.create('draconium_ingot')
-.displayName('Draconium Ingot')
-//Draconium Plate
- event.create('draconium_plate')
-.displayName('Draconium Plate')
-//Draconium Gear
-event.create('draconium_gear')
-.displayName('Draconium Gear')
-
-   //enderIO endergy
-
-//Melodic Alloy
- event.create('melodic_alloy_ingot')
-.displayName('Melodic Alloy Ingot')
-//Crystaline Pink Slime
- event.create('crystalline_pink_slime_ingot').displayName('Crystalline Pink Slime Ingot')
-//Vivid Alloy 
- event.create('vivid_alloy_ingot')
-.displayName('Vivid Alloy Ingot')
-//Energetic Silver Ingot
- event.create('energetic_silver_ingot')
-.displayName('Energetic Silver Ingot')
-//Crystaline Alloy
- event.create('crystalline_alloy_ingot')
-.displayName('Crystalline Alloy Ingot')
-//Stellar Alloy
- event.create('stellar_alloy_ingot')
-.displayName('Stellar Alloy Ingot')
-//Crude Steel
- event.create('crude_steel_ingot')
-.displayName('Crude Steel Ingot')
-// Hot Ferrous pewter
+event.create('zinc_dust').displayName('Zinc Dust')  
+//Hot Ferrous pewter
 event.create('hot_ferrous_pewter_ingot')
 .displayName('Hot Ferrous Pewter Ingot')
 //Ferrous pewter nugget
 event.create('hot_ferrous_pewter_nugget')
 .displayName('Hot Ferrous Pewter nugget')
-//Ferrous Pewter
-event.create('ferrous_pewter_ingot')
-.displayName('Ferrous Pewter Ingot')
-//Ferrous Pewter Plate
-event.create('ferrous_pewter_plate')
-.displayName('Ferrous Pewter Plate')
-//Ferrous Pewter Nugget
-event.create('ferrous_pewter_nugget')
-.displayName('Ferrous Pewter Nugget')
 //Conductive blend
 event.create('conductive_blend')
 .displayName('Conductive Blnd')
@@ -210,51 +132,21 @@ event.create('logistic_machine')
 //Pewter Plate
 event.create('pewter_plate')
 .displayName('Pewter Plate')
-//Pewter Plate
-event.create('dark_steel_plate')
-.displayName('Dark Steel Plate')
 //Diamond Plate
 event.create('diamond_plate')
 .displayName('Diamond Plate')
 //Arcane Runic Slate
 event.create('empty_arcane_runic_slate')
 .displayName('Arcane Runic Slate')
-//Manganese ingot
-event.create('manganese_ingot')
-.displayName('Manganese ingot')
-//Manganese Dust
-event.create('manganese_dust')
-.displayName('Manganese Dust')
-//Tungsten Ingot
-event.create('tungsten_ingot')
-.displayName('Tungsten Ingot')
 //Alloy Mixture
 event.create('alloy_mixture')
 .displayName('Alloy Mixture')
 //Aluminum Plate
 event.create('aluminum_plate')
 .displayName('Aluminum Plate')
-//Arcanite Alloy Ingot
-event.create('arcanite_alloy_ingot')
-.displayName('Arcanite Alloy Ingot')
-//Arcanite Alloy Plate
-event.create('arcanite_alloy_plate')
-.displayName('Arcanite Alloy Plate')
-//Arcanite Alloy Gear
-event.create('arcanite_alloy_gear')
-.displayName('Arcanite Alloy Gear')
 //Arcanite Plate Scrap
 event.create('arcanite_plate_scrap')
 .displayName('Arcanite Plate Scrap ')
-//Orichalcum Ingot
-event.create('orichalcum_ingot')
-.displayName('Orichalcum Ingot')
-//Orichalcum Plate
-event.create('orichalcum_plate')
-.displayName('Orichalcum Plate')
-//Orichalcumy Gear
-event.create('orichalcum_gear')
-.displayName('Orichalcum Gear')
 
 // -x-
 
@@ -276,12 +168,6 @@ event.create('deorum_dust')
 //Deorum dust
 event.create('dusk_world_crystal')
 .displayName('Dusk World Crystal')
-//Compressed Steel Ingot
-event.create('compressed_steel_ingot')
-.displayName('Compressed Steel Ingot')
-//Compressed Steel Plate
-event.create('compressed_steel_plate')
-.displayName('Compressed Steel Plate')
 //Radiant Coil
 event.create('radiant_coil')
 .displayName('Radiant Coil')
