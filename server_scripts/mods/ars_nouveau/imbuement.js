7a62cde68c8e6dd621b56e94aa7640f6d0502d07
BL4CK_ <160023261+imBL4CK@users.noreply.github.com>
2024-05-14 18:34:19 -0300

    remake imbuement

--- server_scripts/mods/ars_nouveau/imbuement.js
+++ server_scripts/mods/ars_nouveau/imbuement.js
@@ -1,32 +1,34 @@
-ServerEvents.recipes(event => {
+ServerEvents.recipes(e => {
     let id = ['ars_nouveau:imbuement/']
-
-    //Fire Essence
-    event.recipes.ars_nouveau.imbuement(
-        ['ars_nouveau:source_gem'],
-        'ars_nouveau:fire_essence',
-        2000,
-        ['quark:soul_bead','minecraft:blaze_powder','minecraft:fire_charge']
-    ).id(id + 'fire_essence')
     //Earth Essence
-    event.recipes.ars_nouveau.imbuement(
+    e.recipes.ars_nouveau.imbuement(
         ['ars_nouveau:source_gem'],
         'ars_nouveau:earth_essence',
         2000,
         ['createaddition:biomass_pellet','thermal:basalz_rod','eidolon:merammer_root']
     ).id(id + 'earth_essence')
     //Water Essence
-    event.recipes.ars_nouveau.imbuement(
+    e.recipes.ars_nouveau.imbuement(
         ['ars_nouveau:source_gem'],
         'ars_nouveau:water_essence',
         2000,
         ['minecraft:water_bucket','thermal:blizz_rod','minecraft:prismarine_shard']
     ).id(id + 'water_essence')
     //Air Essence
-    event.recipes.ars_nouveau.imbuement(
+    e.recipes.ars_nouveau.imbuement(
         ['ars_nouveau:source_gem'],
         'ars_nouveau:air_essence',
         2000,
         ['thermal:blitz_rod', 'ars_nouveau:wilden_wing', 'forbidden_arcanus:golden_feather']
     ).id(id + 'air_essence')
+
+    const recipes = [
+        {
+        //Fire Essence
+        input: 'ars_nouveau:source_gem',
+        output: 'ars_nouveau:fire_essence',
+        source: 1000,
+        reagent: ['quark:soul_bead','minecraft:blaze_powder','minecraft:fire_charge']
+        }
+    ]
 })
